c2468865a1e51a5743b15b651af3881df6faafd0
sshun1n <110231118+sshun1n@users.noreply.github.com>
2024-03-01 23:42:59 +0300

    Add files via upload

---
+++ OOP Polymorpism (01.03.2024)/functions.h
@@ -0,0 +1,264 @@
+﻿#pragma once
+// main includes
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// [ 1ST ]
+class Rational {
+public:
+	// construtor
+	Rational(float num, float denum) : num(num), denum(denum) {}
+	
+	// operators
+	Rational operator +(Rational fr) { Rational temp((this->num * fr.denum + fr.num * this->denum), (this->denum * fr.denum));  return temp; }
+	Rational operator *(Rational fr) { Rational temp((this->num * fr.num), (this->denum * fr.denum)); return temp; }
+
+	void print() { cout << num << "/" << denum; }
+private:
+	// variables
+	float num, denum;
+
+};
+
+// [ 2ND ]
+class Vector {
+public:
+	// constructor
+	Vector(int xCoord, int yCoord) : xCoord(xCoord), yCoord(yCoord) {}
+
+	// operators
+	int operator *(Vector v) { return this->xCoord * v.xCoord + this->yCoord * v.yCoord; }
+
+private:
+	// variables
+	int xCoord, yCoord;
+};
+
+// [ 3RD ]
+class MaxMethods {
+public:
+	int Max(int a, int b) { return max(a, b); }
+	int Max(int a, int b, int c) { return max(a, max(b, c)); }
+	int Max(int a, int b, int c, int d) { return max(a, max(b, max(c, d))); }
+};
+
+// [ 4TH ]
+class AverageMethods {
+public:
+	int Average(vector<int> p) { int sum = 0; for (auto num : p) { sum += num; } return sum / p.size(); }
+	float Average(vector<float> p) { float sum = 0; for (auto num : p) { sum += num; } return sum / p.size(); }
+};
+
+// [ 5TH ]
+class Circle {
+public:
+	// default
+	Circle() : xCenter(0), yCenter(0), radius(0) { cout << "Конструктор без параметров\n"; }
+
+	// customs
+	Circle(float radius) : xCenter(0), yCenter(0), radius(radius) { cout << "Конструктор с 1 параметром\n"; }
+	Circle(float xCenter, float yCenter) : xCenter(xCenter), yCenter(yCenter), radius(0) { cout << "Конструктор с 2 параметрами\n"; }
+	Circle(float xCenter, float yCenter, float radius) : xCenter(xCenter), yCenter(yCenter), radius(radius){ cout << "Конструктор с 3 параметрами\n"; }
+
+private:
+	// variables
+	float xCenter, yCenter, radius;
+};
+
+// [ 7TH ]
+class Vehicle {
+public:
+	// constructor
+	Vehicle() : speed(0) {}
+
+	// virtual method
+	virtual void SpeedUp() {}
+	float getSpeed() { return speed; }
+
+protected:
+	// variables
+	float speed;
+};
+class Car : public Vehicle {
+public:
+	// constructor
+	Car() : Vehicle() {}
+
+	// overrride
+	void SpeedUp() override { speed += 10; }
+};
+class Bicycle : public Vehicle {
+public:
+	// constructor
+	Bicycle() : Vehicle() {}
+
+	// overrride
+	void SpeedUp() override { speed += 3; }
+};
+
+// [ 8TH ]
+class Shape {
+public:
+	// constructor
+	Shape() : a(0), b(0) {}
+	Shape(float a) : a(a), b(0) {}
+	Shape(float a, float b) : a(a), b(b) {}
+
+	virtual float CalculationArea() { return 0; }
+
+protected:
+	// variables
+	float a, b;
+};
+class _Circle : public Shape {
+public:
+	// constructor
+	_Circle(float radius) : Shape(radius) {}
+
+	float CalculationArea() override { return 3.14 * pow(a, 2); }
+
+};
+class Rectangle : public Shape {
+public:
+	// constructor
+	Rectangle(float a, float b) : Shape(a, b) {}
+
+	float CalculationArea() override { return a * b; }
+
+};
+class Triangle : public Shape {
+public:
+	// constructor
+	Triangle(float a, float h) : Shape(a, h) {}
+
+	float CalculationArea() override { return 0.5 * a * b; }
+
+};
+
+// [ 9TH ]
+class Employee {
+protected:
+	// virtual method
+	virtual float CalculateSalary() { return 0; }
+};
+class Manager : protected Employee {
+public:
+	float CalculateSalary() override { return 27500; }
+};
+class Programmer : protected Employee {
+public:
+	float CalculateSalary() override { return 45270; }
+};
+
+// [ 10TH ]
+class Sports {
+protected:
+	virtual void play() const {}
+};
+class Football : protected Sports {
+public:
+	void play() const override { cout << "Играем в футбол.\n"; }
+};
+class Basketball : protected Sports {
+public:
+	void play() const override { cout << "Играем в баскетбол.\n"; }
+};
+class Rugby : protected Sports {
+public:
+	void play() const override { cout << "Играем в регби.\n"; }
+};
+
+// [ 11TH ]
+class Shape1 {
+public:
+	// constructor
+	Shape1() : a(0), b(0) {}
+	Shape1(float a) : a(a), b(0) {}
+	Shape1(float a, float b) : a(a), b(b) {}
+
+	virtual float getArea() { return 0; }
+	virtual float getPerimeter() { return 0; }
+
+protected:
+	// variables
+	float a, b;
+};
+class Circle1 : public Shape1 {
+public:
+	// constructor
+	Circle1(float radius) : Shape1(radius) {}
+
+	float getArea() override { return 3.14 * pow(a, 2); }
+	float getPerimeter() override { return 2 * 3.14 * a; }
+
+};
+class Rectangle1 : public Shape1 {
+public:
+	// constructor
+	Rectangle1(float a, float b) : Shape1(a, b) {}
+
+	float getArea() override { return a * b; }
+	float getPerimeter() override { return (a + b) / 2; }
+
+};
+class Triangle1 : public Shape1 {
+public:
+	// constructor
+	Triangle1(float a, float h) : Shape1(a, h) {}
+
+	float getArea() override { return 0.5 * a * b; }
+	float getPerimeter() override { return (2 * getArea()) / a; }
+
+};
+
+// [ 14TH ]
+class BankAccount {
+public:
+	// constructor
+	BankAccount() : moneyAmount(0) {}
+
+	// methodes
+	void deposit(float moneyAmount) { this->moneyAmount += moneyAmount; }
+	virtual void withdraw(float moneyAmount) {}
+
+protected:
+	// variables
+	float moneyAmount;
+};
+class SavingsAccount : public BankAccount {
+public:
+	void withdraw(float moneyAmount) override {
+		if (moneyAmount > this->moneyAmount) cout << "[Копилка] Ошибка! Средств недостаточно.\n";
+		else { cout << "[Копилка] Коммиссия на снятие - 5%.\nЗапрос: " << moneyAmount << ". Выдано: " << moneyAmount * 0.95 << "."; this->moneyAmount -= moneyAmount; cout << " Остаток: " << this->moneyAmount << ".\n\n"; }
+	}
+};
+class CheckingAccount : public BankAccount {
+public:
+	void withdraw(float moneyAmount) override {
+		if (moneyAmount > this->moneyAmount) cout << "[Расчётный счёт] Ошибка! Средств недостаточно.\n";
+		else { cout << "[Расчётный счёт] Коммиссия на снятие - 3%.\nЗапрос: " << moneyAmount << ". Выдано: " << moneyAmount * 0.97 << "."; this->moneyAmount -= moneyAmount; cout << " Остаток: " << this->moneyAmount << ".\n\n"; }
+	}
+};
+
+// [ 15TH ]
+class _Vehicle {
+protected:
+	// virtual methods
+	virtual void startEngine() const {}
+	virtual void stopEngine() const {}
+};
+class _Car : protected _Vehicle {
+public:
+	// override
+	void startEngine() const override { cout << "[Автомобиль] Двигатель запущен.\n"; }
+	void stopEngine() const override { cout << "[Автомобиль] Двигатель заглушен.\n"; }
+};
+class _Motorcycle : protected _Vehicle {
+public:
+	// override
+	void startEngine() const override { cout << "[Мотоцикл] Двигатель запущен.\n"; }
+	void stopEngine() const override { cout << "[Мотоцикл] Двигатель заглушен.\n"; }
+};
---
+++ OOP Polymorpism (01.03.2024)/main.cpp
@@ -0,0 +1,103 @@
+﻿// main includes
+#include <iostream>
+#include <vector>
+
+// headers
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU"); system("chcp 1251"); system("cls");
+	int inputFunction;
+
+	cout << "Номер задания: "; cin >> inputFunction; system("cls");
+
+	if (inputFunction == 1) {
+		Rational a(1, 2), b(5, 4), temp = a + b;
+		cout << "Сумма: "; temp.print(); temp = a * b;
+		cout << "\nПроизведение: "; temp.print();
+	}
+	if (inputFunction == 2) {
+		Vector a(2, 5), b(3, 7);
+		cout << a * b;
+	}
+	if (inputFunction == 3) {
+		MaxMethods null;
+		cout << null.Max(1, 2) << '\n' << null.Max(1, 2, 3) << '\n' << null.Max(1, 2, 3, 4);
+	}
+	if (inputFunction == 4) {
+		vector<int> a = { 1, 2, 3, 4, 5, 6, 7, 8 }; vector<float> b = { 1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9 };
+		AverageMethods method;
+
+		cout << method.Average(a) << '\n' << method.Average(b);
+	}
+	if (inputFunction == 5) {
+		Circle a; Circle b(10); Circle c(2, 5); Circle(2, 5, 10);
+	}
+	if (inputFunction == 6) { 
+		cout << "Было реализованно ранее."; 
+	}
+	if (inputFunction == 7) {
+		Car car; Bicycle bicycle;
+		cout << "Скорость автомобиля:\n";
+		for (int i = 0; i < 10; i++) {
+			cout << car.getSpeed(); 
+			if (i != 9) cout << ", ";
+			car.SpeedUp();
+		}
+		cout << "\nСкорость велосипеда:\n";
+		for (int i = 0; i < 10; i++) {
+			cout << bicycle.getSpeed(); 
+			if (i != 9) cout << ", ";
+			bicycle.SpeedUp();
+		}
+	}
+	if (inputFunction == 8) {
+		_Circle circle(5); Rectangle rectangle(5, 10); Triangle triangle(5, 6);
+
+		cout << "Площадь круга равна: " << circle.CalculationArea();
+		cout << "\nПлощадь прямоугольника равна: " << rectangle.CalculationArea();
+		cout << "\nПлощадь треугольника равна: " << triangle.CalculationArea();
+	}
+	if (inputFunction == 9) {
+		Manager manager; Programmer programmer;
+
+		cout << "Зарплата менеджера: " << manager.CalculateSalary() << "\nЗарплата программиста: " << programmer.CalculateSalary();
+	}
+	if (inputFunction == 10) {
+		Football footbal; Basketball basketball; Rugby rugby;
+		footbal.play(); basketball.play(); rugby.play();
+	}
+	if (inputFunction == 11) {
+		Circle1 circle(5); Rectangle1 rectangle(5, 10); Triangle1 triangle(5, 6);
+
+		cout << "Площадь круга равна: " << circle.getArea() << " Периметр: " << circle.getPerimeter();
+		cout << "\nПлощадь прямоугольника равна: " << rectangle.getArea() << " Периметр: " << rectangle.getPerimeter();
+		cout << "\nПлощадь треугольника равна: " << triangle.getArea() << " Периметр: " << triangle.getPerimeter();
+	}
+	if (inputFunction == 12) {
+		cout << "Было реализованно ранее.";
+	}
+	if (inputFunction == 13) {
+		cout << "Было реализованно ранее.";
+	}
+	if (inputFunction == 14) {
+		SavingsAccount savings; CheckingAccount checking;
+		savings.deposit(10000); checking.deposit(10000);
+		savings.withdraw(5000); checking.withdraw(7000);
+		savings.withdraw(6000);
+	}
+	if (inputFunction == 15) {
+		_Car car; _Motorcycle bike;
+
+		car.startEngine(); bike.startEngine(); car.stopEngine(); bike.stopEngine();
+	}
+	if (inputFunction == 16) {
+		cout << "Было реализованно ранее.";
+	}
+
+	cout << '\n'; system("pause"); main();
+
+	return 0;
+}

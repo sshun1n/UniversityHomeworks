c63747b9445bb53812d65098a1fa88b18540f56e
sshun1n <110231118+sshun1n@users.noreply.github.com>
2024-03-06 19:10:13 +0300

    Add files via upload

---
+++ Bubble Sorting/functions.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+// main inclusions
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arrayInit(int size) {
+	vector<int> temp(size);
+	for (int i = 0; i < size; i++) temp[i] = rand() % 100 + 1;
+	return temp;
+}
+
+void arrayPrint(vector<int> array) {
+	cout << '[';
+	for (int i = 0; i < array.size(); i++) { cout << array[i]; if (i != array.size() - 1) cout << ", "; }
+	cout << "]\n";
+}
+
+void iteration(vector<int> array, int i, int j) {
+	cout << '\n';
+	for (int i = 0; i < array.size(); i++) { cout << array[i]; if (i != array.size() - 1) cout << ", "; }
+	cout << " | " << i << " <-> " << j;
+}
+
+vector<int> bubbleSort(vector<int> array) {
+	int size = array.size();
+
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = size - 1; j > i; j--) { if (array[j] < array[j - 1]) { swap(array[j], array[j - 1]); iteration(array, array[j], array[j - 1]); } }
+	}
+
+	return array;
+}
+
+vector<int> sinkingSort(vector<int> array) {
+	int size = array.size();
+
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = size - 1; j > i; j--) { if (array[j] < array[j - 1]) { swap(array[j], array[j - 1]); iteration(array, array[j], array[j - 1]); } }
+	}
+
+	return array;
+}
+
+vector<int> qSinkingSort(vector<int> array) {
+	int size = array.size(), i = 0; bool flag = true;
+
+	while (flag) {
+		flag = false;
+		for (int j = size - 1; j > i; j--) if (array[j] < array[j - 1]) { 
+			flag = true; 
+			swap(array[j], array[j - 1]); iteration(array, array[j], array[j - 1]);
+		}
+		i++;
+	}
+
+	return array;
+}
+
+vector<int> shakerSort(vector<int> array) {
+	bool flag = true; int size = array.size(), left = 0, right = size - 1;
+
+	while ((left < right) and flag) {
+		flag = false;
+
+		for (int i = left; i < right; i++) {
+			if (array[i] > array[i + 1]) { flag = true; swap(array[i], array[i + 1]); iteration(array, array[i], array[i + 1]); }
+		} right--;
+
+		for (int j = right; j > left; j--) {
+			if (array[j] < array[j - 1]) { flag = true; swap(array[j], array[j - 1]); iteration(array, array[j], array[j + 1]); }
+		} left++;
+	}
+
+	return array;
+}
---
+++ Bubble Sorting/main.cpp
@@ -0,0 +1,29 @@
+﻿// main inclusions
+#include <iostream>
+#include <vector>
+
+// headers
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU"); system("chcp 1251"); system("cls");
+	int function;
+	cout << "[1] Пузырьковая сортировка\n[2] Сортировка погружения\n[3] Оптимизированный пузырёк\n[4] Шейкерная сортировка\n\nВвод: "; cin >> function; system("cls");
+
+	if (function != 0) {
+		int size; cout << "Введите размерность массива: "; cin >> size; system("cls");
+		vector<int> array = arrayInit(size); cout << "Исходный массив:\n"; arrayPrint(array);
+
+		if (function == 1) array = bubbleSort(array);
+		if (function == 2) array = sinkingSort(array);
+		if (function == 3) array = qSinkingSort(array);
+		if (function == 4) array = shakerSort(array);
+
+		cout << "\n\nОбработанный массив:\n"; arrayPrint(array);
+	}
+
+	cout << '\n'; system("pause");
+	return main();
+}
